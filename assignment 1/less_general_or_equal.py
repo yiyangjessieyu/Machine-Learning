80957b3c9f1945a5addbf74d65a9e2bd5a0813bb
Yiyang (Jessie) Yu <jessieyiyangyu@gmail.com>
2023-03-09 16:21:10 +1300

    less_general_or_equal q4

--- assignment 1/less_general_or_equal.py
+++ assignment 1/less_general_or_equal.py
@@ -1,41 +1,34 @@
 def less_general_or_equal(ha, hb, X):
-
     ha_outputs = [ha(x) for x in X]
     hb_outputs = [hb(x) for x in X]
 
-    # print("ha_output")
-    # for ha_output in ha_outputs:
-    #     print(ha_output)
-    #
-    # print("hb_output")
-    # for hb_output in hb_outputs:
-    #     print(hb_output)
+    return all(hb_outputs[i] == True for i in range(len(X)) if ha_outputs[i] == True)
 
-    return ha_outputs <= hb_outputs and all(ha_x == hb_x for ha_x in ha_outputs for hb_x in hb_outputs if
-                                            ha_x <= hb_x <= ha_x)
 
 X = list(range(1000))
+
 
 def h2(x):
     return x % 2 == 0
 
+
 def h3(x):
     return x % 3 == 0
 
+
 def h6(x):
     return x % 6 == 0
+
 
 H = [h2, h3, h6]
 
 for ha in H:
     for hb in H:
         print(ha.__name__, "<=", hb.__name__, "?", less_general_or_equal(ha, hb, X))
-        ha_outputs = [ha(x) for x in X]
-        hb_outputs = [hb(x) for x in X]
-        print("ha_outputs", sum(ha_outputs))
-        print(ha_outputs)
-        print("hb_outputs", sum(hb_outputs))
-        print(hb_outputs)
-     #   if ha == h3 and hb == h2:
-
-
+        # ha_outputs = [ha(x) for x in X]
+        # hb_outputs = [hb(x) for x in X]
+        # print("ha_outputs", sum(ha_outputs))
+        # print(ha_outputs)
+        # print("hb_outputs", sum(hb_outputs))
+        # print(hb_outputs)
+    #   if ha == h3 and hb == h2:
