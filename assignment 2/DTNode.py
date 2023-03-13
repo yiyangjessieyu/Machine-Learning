70df481344d49498cbe1fbbdb1a87dc304673705
Yiyang (Jessie) Yu <jessieyiyangyu@gmail.com>
2023-03-13 15:45:19 +1300

    q 2) leaves doone

--- assignment 2/DTNode.py
+++ assignment 2/DTNode.py
@@ -19,6 +19,13 @@ class DTNode:
         self.decision = decision
         self.children = children
 
+    def leaves(self):
+        if len(self.children) == 0:
+            return 1
+
+        else:
+            return sum([child_node.leaves() for child_node in self.children])
+
     def predict(self, input_object):
         """
         recursive method
@@ -40,8 +47,6 @@ class DTNode:
 
 # The following (leaf) node will always predict True
 node = DTNode(True)
-
-# Prediction for the input (1, 2, 3):
 x = (1, 2, 3)
 print(node.predict(x))
 
@@ -52,7 +57,6 @@ yes_node = DTNode("Yes")
 no_node = DTNode("No")
 tree_root = DTNode(lambda x: 0 if x[2] < 4 else 1)
 tree_root.children = [yes_node, no_node]
-
 print(tree_root.predict((False, 'Red', 3.5)))
 print(tree_root.predict((False, 'Green', 6.1)))
 
@@ -66,8 +70,42 @@ t.children = [tt, tf]
 f.children = [ft, ff]
 n = DTNode(lambda v: 0 if v[0] else 1)
 n.children = [t, f]
-
 print(n.predict((True, True)))
 print(n.predict((True, False)))
 print(n.predict((False, True)))
 print(n.predict((False, False)))
+
+n = DTNode(True)
+print(n.leaves())
+
+t = DTNode(True)
+f = DTNode(False)
+n = DTNode(lambda v: 0 if not v else 1)
+n.children = [t, f]
+print(n.leaves())
+
+tt = DTNode(False)
+tf = DTNode(True)
+ft = DTNode(True)
+ff = DTNode(False)
+t = DTNode(lambda v: 0 if v[1] else 1)
+f = DTNode(lambda v: 0 if v[1] else 1)
+t.children = [tt, tf]
+f.children = [ft, ff]
+n = DTNode(lambda v: 0 if v[0] else 1)
+n.children = [t, f]
+print(n.leaves())
+
+tt = DTNode(False)
+tf = DTNode(True)
+ft = DTNode(True)
+ff = DTNode(False)
+t = DTNode(lambda v: 0 if v[1] else 1)
+f = DTNode(lambda v: 0 if v[1] else 1)
+t.children = [tt, tf]
+f.children = [ft, ff]
+n = DTNode(lambda v: 0 if v[0] else 1)
+n.children = [t, f]
+z = DTNode(lambda v: 0 if v[0] else 1)
+z.children = [n, n, tt]
+print(z.leaves())
