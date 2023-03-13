bdb465718d44d71e0125e60a5a062effaf039956
Yiyang (Jessie) Yu <jessieyiyangyu@gmail.com>
2023-03-13 17:15:58 +1300

    q4 misclassification WIP

--- assignment 2/misclassification.py
+++ assignment 2/misclassification.py
@@ -3,7 +3,6 @@ All 3 functions take a dataset ((x, y) pairs) and calculate the impurity of the 
 """
 import math
 
-
 def pmk(dataset, i):
     return (1/len(dataset)) * sum([1 for x, y in dataset if x[i] == y])
 
@@ -17,13 +16,25 @@ def gini(dataset):
 
 def entropy(dataset):
     k = len(dataset[0][0])
-    return -1 * sum([pmk(dataset, i) * math.log(pmk(dataset, i)) for i in range(k)])
+    return -1 * sum([pmk(dataset, i) * math.log2(pmk(dataset, i)) for i in range(k)])
 
 data = [
     ((False, False), False),
     ((False, True), True),
     ((True, False), True),
     ((True, True), False)
+]
+print("{:.4f}".format(misclassification(data)))
+print("{:.4f}".format(gini(data)))
+print("{:.4f}".format(entropy(data)))
+
+data = [
+    ((0, 1, 2), 1),
+    ((0, 2, 1), 2),
+    ((1, 0, 2), 1),
+    ((1, 2, 0), 3),
+    ((2, 0, 1), 3),
+    ((2, 1, 0), 3)
 ]
 print("{:.4f}".format(misclassification(data)))
 print("{:.4f}".format(gini(data)))
