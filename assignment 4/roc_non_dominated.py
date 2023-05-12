f5bc225f4c3760ea5527b84255d1d97707080f2a
Yiyang (Jessie) Yu <jessieyiyangyu@gmail.com>
2023-05-12 16:40:10 +1200

    q5

--- assignment 4/roc_non_dominated.py
+++ assignment 4/roc_non_dominated.py
@@ -8,24 +8,22 @@ class ConfusionMatrix(namedtuple("ConfusionMatrix",
 
 
 def roc_non_dominated(classifiers):
-    # TPR_list, FPR_list = [], []
-    #
-    # for name, matrix in classifiers:
-    #     TPR = (matrix.true_positive) / (matrix.true_positive + matrix.false_negative)
-    #     FPR = 1 - TPR
-    #     TPR_list.append(TPR)
-    #     FPR_list.append(FPR)
-    # list of tuples of (TPR, FPR)
-    recall_rates = [((matrix.true_positive) / (matrix.true_positive + matrix.false_negative),
-                     1 - (matrix.true_positive) / (matrix.true_positive + matrix.false_negative))
-                    for name, matrix in classifiers]
-    for i, rates in enumerate(recall_rates):
-        TPR_a, FPR_a = rates
-        other_rates = [other_rate for j, other_rate in enumerate(recall_rates) if i != j]
-        is_dominated = all([TPR_a < TPR_b and FPR_a > FPR_b for TPR_b, FPR_b in other_rates])
-        if is_dominated:
-            del classifiers[i]
-            del recall_rates[i]
+
+    print(len(classifiers))
+
+    if len(classifiers) > 1:
+        print("HERE")
+        recall_rates = [(matrix.true_positive / (matrix.true_positive + matrix.false_negative),
+                         1 - matrix.true_positive / (matrix.true_positive + matrix.false_negative))
+                        for name, matrix in classifiers]
+
+        for i, rates in enumerate(recall_rates):
+            TPR_a, FPR_a = rates
+            other_rates = [other_rate for j, other_rate in enumerate(recall_rates) if i != j]
+            is_dominated = all([TPR_a < TPR_b and FPR_a > FPR_b for TPR_b, FPR_b in other_rates])
+            if is_dominated:
+                del classifiers[i]
+                del recall_rates[i]
 
     return classifiers
 
@@ -39,3 +37,21 @@ classifiers = [
                              50, 50)),
 ]
 print(sorted(label for (label, _) in roc_non_dominated(classifiers)))
+
+classifiers = [
+    ("Just One", ConfusionMatrix(5, 5,
+                                 3, 7)),
+]
+print([l for (l, _) in roc_non_dominated(classifiers)])
+
+
+classifiers = []
+with open("roc_small.data.txt") as f:
+    for line in f.readlines():
+        label, tp, fn, fp, tn = line.strip().split(",")
+        classifiers.append((label,
+                            ConfusionMatrix(int(tp), int(fn),
+                                            int(fp), int(tn))))
+names = [name for name, _ in roc_non_dominated(classifiers)]
+for name in sorted(names):
+    print(name)
